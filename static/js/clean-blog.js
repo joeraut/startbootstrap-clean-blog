d993674beff41c05e517a1be0aa2b47d38f5633d
Joseph Rautenbach <joeraut@users.noreply.github.com>
2019-07-08 10:43:34 +0200

    Disable clearing of fields on contact form send failure

--- static/js/clean-blog.js
+++ static/js/clean-blog.js
@@ -56,8 +56,6 @@ $(function() {
                         .append("</button>");
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!</strong>");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
                 },
             })
         },
